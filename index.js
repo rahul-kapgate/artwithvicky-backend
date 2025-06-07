3fac2ccc762e1fb53e9a8dacee2ddc1047014bce
rahul-sirpi <rahul@sirpi.io>
2025-06-07 16:54:08 +0530

    Fix CORS configuration by removing trailing slash from Netlify origin URL

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://artisticvicky.netlify.app/"], 
+    origin: ["http://localhost:5173", "https://artisticvicky.netlify.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
